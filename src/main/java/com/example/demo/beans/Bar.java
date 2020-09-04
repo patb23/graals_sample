16fbab604ffe4180d51e5b1cf843bf39be87efb5
Pattabi M <patb23@gmail.com>
2020-09-04 01:15:44 -0400

    first

---
+++ src/main/java/com/example/demo/beans/Bar.java
@@ -0,0 +1,11 @@
+package com.example.demo.beans;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Builder;
+
+@Builder
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class Bar {
+
+	String result;
+}
---
+++ src/main/java/com/example/demo/beans/Foo.java
@@ -0,0 +1,9 @@
+package com.example.demo.beans;
+
+import lombok.Data;
+
+@Data
+public class Foo {
+
+	String value;
+}
